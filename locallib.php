dae1eb209879126a8dcc73570690f5ce2fc9ffef
Luca Bösch <luca.boesch@bfh.ch>
2020-04-22 14:21:28 +0200

    Allow localized answer options to be displayed correctly in conditions.

--- locallib.php
+++ locallib.php
@@ -622,7 +622,7 @@ function questionnaire_get_parent ($question) {
         $parent [$qid]['name'] = $question->name;
         $parent [$qid]['content'] = $question->content;
         $parent [$qid]['parentposition'] = $dependquestion->position;
-        $parent [$qid]['parent'] = $dependquestion->name.'->'.$dependchoice;
+        $parent [$qid]['parent'] = format_string($dependquestion->name) . '->' . format_string ($dependchoice);
     }
     return $parent;
 }
